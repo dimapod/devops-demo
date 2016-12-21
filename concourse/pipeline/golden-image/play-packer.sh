59ab7b027fd0cb30ce30da1eff7c9f5e75af4e03
dimapod <dimapod@gmail.com>
2016-12-21 23:17:05 +0100

    Pipeline

---
+++ concourse/pipeline/golden-image/play-packer.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
+
+packer --version
--- concourse/pipeline/golden-image/show_env_var.sh
+++ concourse/pipeline/golden-image/show_env_var.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "Hello ${BLOB_VERSION}    "
+echo "Hello ${BLOB_VERSION}"
