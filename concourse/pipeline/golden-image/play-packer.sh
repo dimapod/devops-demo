aa5f4a39b90698c92e11d86187cb3701309cae4d
dimapod <dimapod@gmail.com>
2016-12-22 22:46:12 +0100

    Echo

--- concourse/pipeline/golden-image/play-packer.sh
+++ concourse/pipeline/golden-image/play-packer.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
-
 cd resource-app/golden-image/base-image
 
 packer build -only amazon-ebs packer-base-image.json | tee output.txt
 
+echo "-----------------------"
 echo "Built image $(tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')"
+echo "-----------------------"
 
 tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../output/base-golden-ami.txt
