92618967589dfb0c6dd7055c3847c9243c13a069
dimapod <dimapod@gmail.com>
2016-12-22 23:01:27 +0100

    Play

--- concourse/pipeline/test/play.sh
+++ concourse/pipeline/test/play.sh
@@ -5,4 +5,13 @@ cd golden-ami-s3/
 ls -l
 cat *
 
+AMI=$(head -n 1 base-golden-ami.txt)
+VERSION=$(head -n 1 version)
+
+echo "Using golden base image: ${AMI} version ${VERSION}"
+
+export AMI_BASE_IMAGE=${AMI}
+
+echo "exported : ${AMI_BASE_IMAGE}"
+
 
