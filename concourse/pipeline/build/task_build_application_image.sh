a3264b5d4d90c9ec025c04fb8fd3309733910a04
dimapod <dimapod@gmail.com>
2016-12-22 23:39:50 +0100

    Application image

--- concourse/pipeline/build/task_build_application_image.sh
+++ concourse/pipeline/build/task_build_application_image.sh
@@ -7,7 +7,7 @@ echo "Using golden base image: ${AMI}"
 export AMI_BASE_IMAGE=${AMI}
 
 # Copy built artifact
-cp gs-bundle/target/gs-spring-boot-*.jar resource-app/golden-image/application-image/gs-spring-boot/target/
+cp my-product-rc/target/gs-spring-boot-*.jar resource-app/golden-image/application-image/gs-spring-boot/target/
 
 
 cd resource-app/golden-image/application-image
