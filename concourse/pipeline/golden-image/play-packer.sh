d336b7201128dd919a8a9f0a57c5ad8b9e504fe0
dimapod <dimapod@gmail.com>
2016-12-22 21:31:47 +0100

    Refactoring

--- concourse/pipeline/golden-image/play-packer.sh
+++ concourse/pipeline/golden-image/play-packer.sh
@@ -2,10 +2,10 @@
 
 echo "AWS_ACCESS_KEY_ID: ${AWS_ACCESS_KEY_ID}"
 
-cd resource-app/packer
+cd golden-image/base-image
 
-packer build -only amazon-ebs packer-server.json | tee output.txt
+packer build -only amazon-ebs packer-base-image.json | tee output.txt
 
 echo "Built image $(tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')"
 
-tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../output/golden-ami.txt
+tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../output/base-golden-ami-0.1.0.txt
