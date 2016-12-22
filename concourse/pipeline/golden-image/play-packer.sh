c60e913baec94123d78820c7b7b4ffd49614dc55
dimapod <dimapod@gmail.com>
2016-12-22 22:39:47 +0100

    Fix fetching AMI ID

--- concourse/pipeline/golden-image/play-packer.sh
+++ concourse/pipeline/golden-image/play-packer.sh
@@ -8,4 +8,4 @@ packer build -only amazon-ebs packer-base-image.json | tee output.txt
 
 echo "Built image $(tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')"
 
-tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../output/base-golden-ami-0.1.0.txt
+tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../output/base-golden-ami-0.1.0.txt
