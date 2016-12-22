e0eba7df41baf81a211cb2581d11d5fbe9d8ba3a
dimapod <dimapod@gmail.com>
2016-12-22 23:17:22 +0100

    Application image

---
+++ concourse/pipeline/build/task_build_application_image.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Prepare Base AMI ID
+AMI=$(head -n 1 golden-ami-s3/base-golden-ami.txt)
+VERSION=$(head -n 1 golden-ami-s3/version)
+echo "Using golden base image: ${AMI}"
+export AMI_BASE_IMAGE=${AMI}
+
+# Copy built artifact
+cp gs-bundle/target/gs-spring-boot-*.jar resource-app/golden-image/application-image/gs-spring-boot/target/
+
+
+cd resource-app/golden-image/application-image
+
+packer build -only amazon-ebs packer-application-image.json | tee output.txt
+
+echo "-----------------------"
+echo "Built image $(tail -3 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')"
+echo "-----------------------"
+
+tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../golden-application-image/application-golden-ami.txt
+
