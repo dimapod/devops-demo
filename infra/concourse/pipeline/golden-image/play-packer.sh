1a635582bed466fbade1dc1bd3cb7a9bf693bae9
dimapod <dimapod@gmail.com>
2016-12-25 18:59:49 +0100

    Fix path

--- infra/concourse/pipeline/golden-image/play-packer.sh
+++ infra/concourse/pipeline/golden-image/play-packer.sh
@@ -8,4 +8,4 @@ echo "-----------------------"
 echo "Built image $(tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }')"
 echo "-----------------------"
 
-tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../output/base-image-id
+tail -2 output.txt | head -1 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' > ../../../../output/base-image-id
