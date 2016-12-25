c69d8df13bad38912deba007e51b44a0b5313ed0
dimapod <dimapod@gmail.com>
2016-12-25 15:54:59 +0100

    Terraform start / stop infra

--- concourse/pipeline/build/task_deploy.sh
+++ concourse/pipeline/build/task_deploy.sh
@@ -4,17 +4,21 @@
 AMI=$(head -n 1 application-image-s3/application-image-id)
 VERSION=$(head -n 1 application-image-s3/version)
 
+# Prepare previous infrastructure description
+mv devops-infra/infra-as-code/terraform.tfstate devops-infra/infra-as-code/terraform.tfstate_old
+cp infra-description-s3/terraform.tfstate devops-infra/infra-as-code/
+
+# Prepare AWS Keys
 echo "Using application image: ${AMI}"
 export TF_VAR_access_key=${AWS_ACCESS_KEY_ID}
 export TF_VAR_secret_key=${AWS_SECRET_ACCESS_KEY}
 export TF_VAR_application_ami=${AMI}
-
 set | grep TF_VAR
 
+# Plan/Apply terraform
 cd devops-infra/infra-as-code
-
-terraform -v
 terraform plan
 terraform apply
 
+# Copy current infrastructure description
 cp terraform.tfstate ../../infra-description/
