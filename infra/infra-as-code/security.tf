3e9c07d00c249b245b23a522c3c0908f1fbf76f9
dimapod <dimapod@gmail.com>
2017-01-12 21:29:19 +0100

    Add ingress 9100 on app infra

--- infra/infra-as-code/security.tf
+++ infra/infra-as-code/security.tf
@@ -6,16 +6,21 @@ resource "aws_security_group" "default" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = [
-      "0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 9100
+    to_port = 9100
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
